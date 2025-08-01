021207e3bcfb3bd84c1e24d3f6b3cb561033c4a6
anhcd <chuducanhwork@gmail.com>
2025-08-02 00:30:46 +0700

    first test commit for first blog

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -1,14 +1,29 @@
 #!/usr/bin/env ruby
-#
-# Check for changed posts
+# frozen_string_literal: true
+
+require "jekyll"
+require "pathname"
+require "open3"
 
 Jekyll::Hooks.register :posts, :post_init do |post|
+  # 1. Tính đường dẫn tương đối từ repo root
+  repo_root = Pathname.new(Dir.pwd)
+  rel_path  = Pathname
+                  .new(post.path)
+                  .relative_path_from(repo_root)
+                  .to_s
+                  .tr("\\", "/")  # đảm bảo dùng slash
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  # 2. Đếm commit qua Open3.capture2 (trả về [stdout, status])
+  commit_num, _status = Open3.capture2(
+    "git", "rev-list", "--count", "HEAD", rel_path
+  )
 
   if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+    # 3. Lấy last modified date cũng qua capture2
+    lastmod, _status2 = Open3.capture2(
+      "git", "log", "-1", "--pretty=%ad", "--date=iso", rel_path
+    )
+    post.data["last_modified_at"] = lastmod.strip
   end
-
 end
